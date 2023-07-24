adc8f6cdc947d7da2aee800f53941287b6739f49
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-24 23:37:54 +0530

    added css to meetup details

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Fragment } from 'react'
+import classes from './MeetupDetails.module.css'
 
 const MeetupDetails = (props) => {
   return (
-    <Fragment>
+    <section className={classes.detail}>
+
 <img 
 src={props.image}
 alt={props.title}
@@ -11,7 +13,8 @@ alt={props.title}
 <h1>{props.title}</h1>
 <address>{props.address}</address>
 <p>{props.description}</p>
-</Fragment>
+</section>
+
   )
 }
 
